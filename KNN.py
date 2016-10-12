12a6936d6f0759a040eace8d17d55ac47623403f
lianqiyan <lianqiyan@outlook.com>
2016-10-12 17:02:43 +0800

    Update KNN.py
    convert the codes  into a function, add support classification of multiple samples

--- KNN.py
+++ KNN.py
@@ -2,16 +2,24 @@ import numpy as np
 import operator
 import matplotlib as plt
 
-T_data = np.array([[1, 1.1], [1, 1], [0, 0], [0, 0.1]])
-T_labels = ['A', 'A', 'B', 'B']
+p_data = np.array([[1, 1.1], [1, 1], [0, 0], [0, 0.1]])
+p_labels = ['A', 'A', 'B', 'B']
 
-c_data = np.array([0, 0.2])
-mdiff = np.tile(c_data, (T_data.shape[0], T_data.shape[1]-1)) - T_data
-dist = (pow((pow(mdiff, 2).sum(axis=1)), 0.5))
-or_index = np.argsort(dist)
-num_lab = {}
-for i in range(3):
-    vlab = T_labels[or_index[i]]
-    num_lab[vlab] = num_lab.get(vlab, 0) + 1
-s_num = sorted(num_lab.items(), key=operator.itemgetter(1), reverse=True)
-print(s_num[0])
+
+def knn_classify(T_data, T_labels, k, c_data):
+    c_labels = []
+    for i in range(c_data.shape[0]):
+        temp = c_data[i][:]
+        mdiff = np.tile(temp, (T_data.shape[0], T_data.shape[1] - 1)) - T_data
+        dist = (pow((pow(mdiff, 2).sum(axis=1)), 0.5))
+        or_index = np.argsort(dist)
+        num_lab = {}
+        for i in range(k):
+            vlab = T_labels[or_index[i]]
+            num_lab[vlab] = num_lab.get(vlab, 0) + 1
+        s_num = sorted(num_lab.items(), key=operator.itemgetter(1), reverse=True)
+        c_labels.append(s_num[0][0])
+    return c_labels
+
+t_data = np.array([[1.2, 0.8], [-0.1, 0.3]])
+print(knn_classify(p_data, p_labels, 3, t_data))
